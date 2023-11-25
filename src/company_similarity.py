0104a97344c4d60d4630b970ae3f9b5447060e4a
angelina <ashauswald@gmail.com>
2023-11-25 17:04:09 +0100

    use sys to hand over data via command line

--- src/company_similarity.py
+++ src/company_similarity.py
@@ -1,9 +1,17 @@
+import sys
 import json
 
-def read_json():
-    file = open('data/company_collection.json')
+def read_json(input_file):
+    # TODO: check if file exists
+    file = open(input_file)
     data = json.load(file)
     file.close()
+    return data
 
 if __name__ == "__main__":
-    read_json()
+    if len(sys.argv) < 2:
+        # TODO: pretty error messages
+        print("Missing path to data file")
+    else:
+        input_file = sys.argv[1]
+        print(read_json(input_file))
